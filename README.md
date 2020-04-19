a8498b9e30f6d5f3e76384d71351bd0a96b1be27
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2020-04-19 16:36:12 +0100

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # resonance
-An experimental audio synchronizer backend in Rust
+
+An experimental audio synchronizer backend built using websockets.
+
+This backend allows any audio or video source to be synchronized between clients.
+
+## Safety
+
+This application declares ``#![forbid(unsafe_code)]``.
--- resonance/src/main.rs
+++ resonance/src/main.rs
@@ -1,11 +1,12 @@
 //! # resonance
 //!
 //! An experimental audio synchronizer backend built using websockets.
+//! 
 //! This backend allows any audio or video source to be synchronized between clients.
 //!
 //! ## Safety
 //!
-//! This application declares ``#![forbid(unsafe_code)]``
+//! This application declares ``#![forbid(unsafe_code)]``.
 
 #![forbid(unsafe_code)]
 
