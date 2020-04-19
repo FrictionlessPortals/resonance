9a42b7df1833cbe333f47d85139395eb2ebb1f1e
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2020-04-19 16:48:33 +0100

    Init harmonic module

---
+++ harmonic/src/lib.rs
@@ -0,0 +1,9 @@
+//! # harmonic
+//!
+//! The API implementations and definitions for ``resonance``.
+//!
+//! ## Safety
+//!
+//! This library declares ``#![forbid(unsafe_code)]``.
+
+#![forbid(unsafe_code)]
