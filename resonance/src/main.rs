4eeeaa6936ab284ba8fb1f721d03ba38759481ef
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2020-04-19 16:34:36 +0100

    Add basic websocket endpoint

--- resonance/src/main.rs
+++ resonance/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+//! # resonance
+//!
+//! An experimental audio synchronizer backend built using websockets.
+//! This backend allows any audio or video source to be synchronized between clients.
+//!
+//! ## Safety
+//!
+//! This application declares ``#![forbid(unsafe_code)]``
+
+#![forbid(unsafe_code)]
+
+use futures::{FutureExt, StreamExt};
+use log::error;
+use std::error::Error;
+use warp::{ws::Ws, Filter};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Setup environment variable based logging.
+    pretty_env_logger::init();
+
+    // Websocket API connection route.
+    let connection = warp::any().and(warp::ws()).map(|ws: Ws| {
+        ws.on_upgrade(move |socket| {
+            let (tx, rx) = socket.split();
+            rx.forward(tx).map(|result| {
+                if let Err(e) = result {
+                    error!("websocket error: {:?}", e);
+                }
+            })
+        })
+    });
+
+    // Serve the websocket connection route.
+    warp::serve(connection).run(([127, 0, 0, 1], 3030)).await;
+
+    Ok(())
 }
